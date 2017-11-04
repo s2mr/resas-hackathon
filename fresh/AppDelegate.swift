7c616ca1357707172747ec8ba171c6dd510dc474
Kazumasa Shimomura <kazu.devapp@gmail.com>
2017-11-04 17:52:28 +0900

    [add]GenreViewController

--- fresh/AppDelegate.swift
+++ fresh/AppDelegate.swift
@@ -16,9 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
-		
-		let a = try! CityDataStore().getCities()
-		print(a.cities)
 		return true
 	}
 
--- fresh/ViewController/Root/RootController.swift
+++ fresh/ViewController/Root/RootController.swift
@@ -19,7 +19,7 @@ class RootController: UITabBarController {
 		let profileTop = UIStoryboard(name: "ProfileTopViewController", bundle: nil).instantiateInitialViewController()!
 		profileTop.title = "プロフィール"
 		
-		self.viewControllers = [quizTop, profileTop]
+		self.viewControllers = [UINavigationController(rootViewController: quizTop), profileTop]
 		
 		// Do any additional setup after loading the view.
 	}
