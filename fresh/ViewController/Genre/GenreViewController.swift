bf670f520028ce2a4187d77a018dc44669c91e34
Kazumasa Shimomura <kazu.devapp@gmail.com>
2017-11-04 20:18:45 +0900

    [modify]show genre

--- fresh/ViewController/Genre/GenreViewController.swift
+++ fresh/ViewController/Genre/GenreViewController.swift
@@ -9,27 +9,50 @@
 import UIKit
 
 class GenreViewController: UIViewController {
+	
+	@IBOutlet weak var collectionView: UICollectionView!
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Do any additional setup after loading the view.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	
+	/*
+	// MARK: - Navigation
+	
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+	// Get the new view controller using segue.destinationViewController.
+	// Pass the selected object to the new view controller.
+	}
+	*/
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+extension GenreViewController: UICollectionViewDataSource {
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! GenreCollectionViewCell
+		cell.image.image = UIImage(named: "genre-yama")
+		cell.nameLabel.text = "山"
+		return cell
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 3
+	}
+}
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+extension GenreViewController: UICollectionViewDelegateFlowLayout {
+	// 希望のセルサイズを返す
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		let size = self.view.frame.size.width/2-10
+		return CGSize(width: size, height: size)
+	}
 }
